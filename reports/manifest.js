57d8f6a5cdedf35345d04f81c7b26dc7a79a44ce
CraneDH <105138312+luckyDH518@users.noreply.github.com>
2026-08-28 10:02:57 +0800

    Update research report manifest for 2026-08-28

--- reports/manifest.js
+++ reports/manifest.js
@@ -34,5 +34,6 @@ window.RESEARCH_REPORT_SOURCES = [
   'reports/2026-08/2026-08-24.js',
   'reports/2026-08/2026-08-25.js',
   'reports/2026-08/2026-08-26.js',
-  'reports/2026-08/2026-08-27.js'
+  'reports/2026-08/2026-08-27.js',
+  'reports/2026-08/2026-08-28.js'
 ];
