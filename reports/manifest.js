5a40c649a2d36ef590de44ac4721086f5fb87f76
CraneDH <105138312+luckyDH518@users.noreply.github.com>
2026-09-04 10:03:10 +0800

    Register 2026-09-04 research daily

--- reports/manifest.js
+++ reports/manifest.js
@@ -39,5 +39,6 @@ window.RESEARCH_REPORT_SOURCES = [
   'reports/2026-08/2026-08-31.js',
   'reports/2026-09/2026-09-01.js',
   'reports/2026-09/2026-09-02.js',
-  'reports/2026-09/2026-09-03.js'
+  'reports/2026-09/2026-09-03.js',
+  'reports/2026-09/2026-09-04.js'
 ];
